8f060274cf8078c50f2ca786a792141ad57636bd
Barbara <barbara.estevez@gmail.com>
2021-05-12 10:24:25 +0200

    add some js

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const inputElement = document.querySelector('.js-input');
+const submitElement = document.querySelector('.js-submit');
+const resultElement = document.querySelector('.js-result');
+const inputValue = inputElement;
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+
+console.log(getRandomNumber);
