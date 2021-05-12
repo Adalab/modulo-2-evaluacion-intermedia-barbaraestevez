7fb76568cf69b7f707b737c1335177bd05346bd7
Barbara <barbara.estevez@gmail.com>
2021-05-12 11:09:31 +0200

    add some new js

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,38 @@
 'use strict';
 
 const inputElement = document.querySelector('.js-input');
-const submitElement = document.querySelector('.js-submit');
+const submitElement = document.querySelector('.js-button');
 const resultElement = document.querySelector('.js-result');
-const inputValue = inputElement;
+const hint = document.querySelector('.js-hint');
+const counter = document.querySelector('.js-trials');
+const randomNumber = getRandomNumber(100);
 
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
     }
 
-console.log(getRandomNumber);
+function checkNumber () {
+    const number = parseInt(inputElement.value);
+    
+if (number < 1 || number > 100) {
+    hint.innerHTML = "Pista: El número debe estar entre 1 y 100";
+} else if (number < randomNumber) {
+    hint.innerHTML = "Pista: Demasiado bajo";
+  } else if (number > randomNumber) {
+    hint.innerHTML = "Pista: Demasiado alto";
+  } else (number === randomNumber) {
+    hint.innerHTML = "Has ganado, campeona!!!";
+  } else {
+    text.innerHTML = "El número debe estar entre 1 y 100";
+  }
+  increaseCounter();
+}
+function increaseCounter () {
+    count += 1;
+    counter.value = `Número de intentos: ${count}`;
+}
+function handleClick () {
+    checkNum ();
+}
+button.addEventListener ('click', handleClick);
